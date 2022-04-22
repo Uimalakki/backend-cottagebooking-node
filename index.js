ce1b7e2a6c82182f2fe7a8259eb1b24df8803c74
Uimalakki <bjupi001@edu.xamk.fi>
2022-04-22 10:05:41 +0300

    refactored index.js

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ const express = require('express')
 const app = express()
 const cors = require('cors')
 const cottagesRouter = require('./controllers/cottages')
+const bookingsRouter = require('./controllers/bookings')
 const logger = require('./utils/logger')
 const mongoose = require('mongoose')
 const middleware = require('./utils/middleware')
@@ -22,61 +23,12 @@ app.use(cors())
 app.use(express.json())
 
 app.use('/api/cottages', cottagesRouter)
-
-let cottages = [
-  {
-    id: 1,
-    name: 'Metso',
-    price: 150
-  },
-  {
-    id: 2,
-    name: 'Kuukkeli',
-    price: 125
-  },
-  {
-    id: 3,
-    name: 'Kaakkuri',
-    price: 99
-  },
-  {
-    id: 4,
-    name: 'Sikotauti',
-    price: 50
-  }
-]
+app.use('/api/bookings', bookingsRouter)
 
 app.get('/', (request, response) => {
   response.send('<h1>Cottagebooking backend</h1>')
 })
-/*
-app.get('/api/cottages', (request, response) => {
-  response.json(cottages)
-})
 
-app.get('/api/cottages/:id', (request, response) => {
-  const id = Number(request.params.id)
-  const cottage = cottages.find(cottage => cottage.id === id)
-
-  if(cottage) {
-    response.json(cottage)
-  } else {
-    response.status(404).end()
-  }
-})
-
-app.delete('/api/cottages/:id', (request, response) => {
-  const id = Number(request.params.id)
-  cottages = cottages.filter(cottage => cottage.id !== id)
-
-  response.status(204).end()
-})
-
-app.post('/api/cottages', (request, response) => {
-  const cottage = request.body
-  response.json(cottage)
-})
-*/
 app.use(middleware.errorHandler)
 
 app.listen(config.PORT)
